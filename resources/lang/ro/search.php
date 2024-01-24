034d940a964c4989b5806b0b08da9f2585f84169
Vadik <muzcafe@gmail.com>
2024-01-24 14:18:15 +0200

    translation search

---
+++ resources/lang/ro/search.php
@@ -0,0 +1,6 @@
+<?php
+return[
+    'results' => 'Rezultatul căutării:',
+    'total_products' => 'Produse găsite:',
+    'no_results' => 'Nu a fost găsit nimic!!!',
+];
--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.layouts')
 
-@section('title', 'Rezultatul căutării:' .$query)
+@section('title', __('search.results'). '' .$query)
 
 @section('content')
     <div class="wrapper">
         @if($products->isNotEmpty())
-            <h1>Rezultatul căutării: {{ $query }}</h1>
-            <small>Produse găsite: {{ $products_count }}</small>
+            <h1>@lang('search.results') {{ $query }}</h1>
+            <small>@lang('search.total_products') {{ $products_count }}</small>
             @include('block.product_list')
         @else
             <div align="center">
                 <img style="max-width: 100%;" src="/img/cat1.jpg" alt="Oops...">
-                <h2>Nu a fost găsit nimic!!!</h2>
+                <h2>@lang('search.no_results')</h2>
             </div>
         @endif
 {{--        <div class="d-flex justify-content-center" style="padding-top: 10px;">
