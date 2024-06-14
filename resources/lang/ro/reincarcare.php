43943fc84e1776886c483f0efb83bcbaaf965b4d
vadik <muzcafe@gmail.com>
2024-06-14 11:36:09 +0300

    Locale reincarcari

---
+++ resources/lang/ro/reincarcare.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    'title' => 'Reîncărcarea și regenerarea cartușelor în Chișinău, Moldova',
+    'desc' => 'Reîncărcarea și regenerarea cartușelor în sectorul Botanica, Chișinău, Moldova',
+    'key' => 'reincarcarea cartuselor, reincarcarea imprimantelor, laser, toner, injet, alimentarea cartuselor, alimentarea imprimantelor, chisinau, moldova, reincarcarea cartuselor chisinau, reincarcarea caruselor laser, reincarcarea cartuselor canon, lasejet, samsung, hp, canon',
+    'h1' => 'Reîncărcarea și reparația cartușelor',
+    't1' => 'vă oferă servicii de reîncărcare și regenerare a cartușelor pentru imprimantele Dumneavoastră:',
+    't2' => 'reîncărcare cartușe inkjet (cerneală)',
+    't3' => 'reîncărcare cartușe laserjet (toner)',
+    't4' => 'regenerare cartușe inkjet (cerneală)',
+    't5' => 'regenerare cartușe laserjet (toner)',
+    't6' => 'Cand imprimanta rămîne fără cerneală sau toner în cartușe, veți fi surprinși să aflați că pentru
+                achiziționarea altor cartușe noi achitați aproape încă o dată prețul imprimantei. Compania noastră are
+                tehnologia necesară pentru reîncărcarea și regenerarea cartușelor rapid și eficient la doar o parte din
+                costul unui cartuș original.',
+    't7' => 'Prin reîncărcarea cartușelor ink (cerneală) economisiți pană la 90%, iar prin reîncărcarea cartușelor laser (cartușe cu toner) aproximativ 80% .',
+    'h2' => 'Reîncărcarea cartușelor laser de la 80 lei',
+    't8' => 'Cartușele pentru imprimante sunt consumabile destinate pentru unica folosință și nimeni nu poate da
+                garanție la calitatea tiparului după reincărcarea lor. Totul depinde de viteza de uzare a componenților
+                cartușului care variază de la un model la altul, cel mai repede se uzează fotoreceptorul, lamele și
+                valurile
+                care es din funcțiune deobicei după 3-4 reîncărcări după care este necesar de făcut regenerarea
+                cartușului.',
+    't9' => 'Regenerarea cartușului constă în reîncărcarea cu toner și schimbarea tuturor componentiolor uzate din
+                cartuș.
+                Regeneraea cartușilui original poate fi efectuată în medie în jur de 5 ori, deobicei totul depinde de
+                starea
+                carcacasei cartușului și a producătorului, după care este necesar de procurat alt cartuș nou.',
+    'h3' => 'Reîncărcarea cartușelor cu vopsea de la 30 lei',
+];
--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name') }} - @yield('title')</title>
+    <title>@yield('title') - {{ config('app.name') }}</title>
     <meta name="description" content="@yield('description')"/>
     <meta name="keywords" content="@yield('keywords')"/>
     <meta property="og:title" content="@yield('title')" />
