879b6572775fcd2e58230cf5a256b75b5830c496
Vadik <muzcafe@gmail.com>
2025-02-17 10:10:16 +0200

    Add maps

--- resources/views/block/maps.blade.php
+++ resources/views/block/maps.blade.php
@@ -1,4 +1,4 @@
 <iframe
-    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1717.8965182409609!2d28.884181983042637!3d46.97943359075496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sru!2s!4v1671281253039!5m2!1sru!2s"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3377.811850072484!2d28.88139437678703!3d46.97940753107701!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c97969eceac7f1%3A0xf631ea875452644d!2sComp.MD!5e1!3m2!1sro!2s!4v1739779757488!5m2!1sro!2s"
     width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy"
     referrerpolicy="no-referrer-when-downgrade"></iframe>
